526603526c9ae6b8584473cc995bfadf01185905
M@D <80210946+ZuleMD@users.noreply.github.com>
2023-03-20 01:42:25 +0100

    Create myLog.java

---
+++ myLog.java
@@ -0,0 +1,9 @@
+import android.util.Log;
+
+public class MyClass {
+    private static final String TAG = "MyClass";
+
+    public void myMethod() {
+        Log.i(TAG, "This is a log message");
+    }
+}
